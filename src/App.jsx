9020070fc078dbdc05c56a56321879c2aef3e9eb
Izma-ali <alimuhammadsameed@gmail.com>
2024-12-16 08:40:05 +0500

    added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Cards from './components/Cards'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Cards/>
+    </div>
+  )
+}
